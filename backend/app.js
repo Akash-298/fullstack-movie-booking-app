96e70db0f47e17256dea0ca7f80c2f3bd1ba11ab
Akash-298 <akash.c.c298@gmail.com>
2023-08-29 15:09:42 +0530

    get alll the user

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,27 @@
 import  express  from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routes/user-router";
+
+dotenv.config();
 const app = express();
 
+// middlewears
+  app.use("/user", userRouter);
 
-
-app.use("/", (req,res,next)=>{
-    res.send("h");
-});
-
-
- app.listen(5000, () =>{
+mongoose
+  .connect(
+    `mongodb+srv://akash:${process.env.MONGODB_PASSWORD}@cluster0.pkknmce.mongodb.net/?retryWrites=true&w=majority`
+  )
+  .then(() =>
+    app.listen(5000, () =>
       console.log("Connected To Database And Server is running")
-     } )
+    )
+  )
+  .catch((e) => console.log(e));
+
+
+
+
+
   
---
+++ backend/routes copy/user-router.js
@@ -0,0 +1,14 @@
+import express from "express";
+import { getAllUsers } from "../controller/user-controller";
+
+const userRouter = express.Router();
+
+userRouter.get("/", getAllUsers);
+// userRouter.get("/:id", getUserById);
+// userRouter.post("/signup", singup);
+// userRouter.put("/:id", updateUser);
+// userRouter.delete("/:id", deleteUser);
+// userRouter.post("/login", login);
+// userRouter.get("/bookings/:id", getBookingsOfUser);
+
+export default userRouter;
