5539168d7b9e26c79691fc6705b1e9d4d11b782f
Akash-298 <akash.c.c298@gmail.com>
2023-09-01 06:43:40 +0530

    Booking Functionality and page

--- movies/src/App.js
+++ movies/src/App.js
@@ -7,6 +7,7 @@ import { Route, Routes } from "react-router-dom";
 import Auth from "./components/Auth/Auth";
 import { useDispatch, useSelector } from "react-redux";
 import { adminActions, userActions } from "./store";
+import Booking from "./components/Bookings/Booking";
 
 
 function App() {
@@ -31,6 +32,7 @@ function App() {
           <Route path="/movies" element={<Movies />} />
           <Route path="/admin" element={<Admin />} />
           <Route path="/auth" element={<Auth />} />
+          <Route path="/booking/:id" element={<Booking />} />
          
         </Routes>
       </section> 
