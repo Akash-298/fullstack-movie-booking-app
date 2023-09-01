8f175fd867c70e8e67984191b0287e832dd62030
AKASH CHIKHALONDE <105929312+Akash-298@users.noreply.github.com>
2023-09-01 14:21:37 +0530

    Delete backend directory

--- backend/routes/booking-router.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { deleteBooking, getBookingById, newBooking } from "../controller/booking-controller";
-
-const bookingsRouter = express.Router();
-
-bookingsRouter.get("/:id", getBookingById);
-bookingsRouter.post("/", newBooking);
-bookingsRouter.delete("/:id", deleteBooking);
-export default bookingsRouter;
--- backend/routes/movie-router.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { addMovie, getAllMovies, getMovieById } from "../controller/movie-controller";
-
-const movieRouter = express.Router();
-movieRouter.get("/", getAllMovies);
-movieRouter.get("/:id", getMovieById);
-movieRouter.post("/", addMovie);
-
-export default movieRouter;
